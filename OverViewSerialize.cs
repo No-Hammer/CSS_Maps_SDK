04dcd56842b99b3a937a58e98d6f2a7abb5b9a15
No_Hammer <31363294+No-Hammer@users.noreply.github.com>
2022-09-30 19:01:54 +0500

    Update 6.15.0 BugFix

---
+++ OverViewSerialize.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class OverViewSerialize : MonoBehaviour
+{
+    public class OverView
+    {
+        public string Texture;
+        public float PosX;
+        public float PosZ;
+        public float Width;
+        public float Height;
+    }
+    
+#if UNITY_EDITOR
+    public static void Save(string mapName, OverView overview)
+    {
+        string fullPath = $"{Path.GetFullPath(Path.Combine(Application.dataPath, "../"))}/maps/overviews/{mapName}";
+        if (File.Exists(fullPath))
+            File.Delete(fullPath);
+        using (FileStream fs = new FileStream(fullPath, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+        {
+            using (StreamWriter writer = new StreamWriter(fs))
+            {
+                writer.WriteLine(JsonUtility.ToJson(overview, true));
+            }
+        }
+    }
+#endif
+}
